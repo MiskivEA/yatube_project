d1f37943fe58baae37d4f4aff7e6a7ecccc7b8f9
Евгений Миськив <evgenymiskiv@gmail.com>
2022-08-23 20:44:32 +0300

    Добавлены шаблоны

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     #home page
-    path('', views.index),
+    path('', views.index, name = 'group_list'),
     #пока не знаю
-    path('group/<slug:param>', views.group_posts)
+    path('group_list/', views.group_list, name = 'group_list'),
+    path('group/<slug:param>', views.group_posts, name = 'group_list')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,20 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_list(request):
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, 'posts/group_list.html', context)
 
 def group_posts(request, param):
-    return HttpResponse(f'Страница {param}')
+    return HttpResponse(f'{param} it will be later')
