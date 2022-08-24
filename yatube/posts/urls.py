758596d6dcdab464aeca578738f0ebf37df500db
Евгений Миськив <evgenymiskiv@gmail.com>
2022-08-24 11:53:26 +0300

    Add link logo

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,8 +6,8 @@ app_name = 'posts'
 
 urlpatterns = [
     #home page
-    path('', views.index, name = 'group_list'),
+    path('', views.index, name = 'home_page'),
     #пока не знаю
     path('group_list/', views.group_list, name = 'group_list'),
-    path('group/<slug:param>', views.group_posts, name = 'group_list')
+    path('group/<slug:param>', views.group_posts, name = 'group_list_param')
 ]
